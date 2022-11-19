855f74d2d0e2ec3dae2405cb58ec647bff7957ab
avnogy <avner.mindelis@gmail.com>
2022-11-19 22:14:52 +0200

    updated poc to include multiple messages from server

--- demo/client.py
+++ demo/client.py
@@ -8,5 +8,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b"Hello, world")
     data = s.recv(1024)
-
-print(f"Received {data.decode()}")
+    while data:
+        print(f"Received {data.decode()}")
+        data = s.recv(1024)
--- demo/server.py
+++ demo/server.py
@@ -4,17 +4,23 @@ import subprocess
 subprocess.Popen(["upnpc.exe", "-a","192.168.123.43","2222","7777","tcp"])
 HOST = "192.168.123.43"
 PORT = 2222
+try:
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected to {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-subprocess.Popen(["upnpc.exe", "-d","7777","tcp"])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected to {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                while data != '':
+                    data = input()
+                    conn.sendall(data.encode())
+                    
+except Exception as e:
+    print("Error:", e)
+finally:
+    subprocess.Popen(["upnpc.exe", "-d","7777","tcp"])
