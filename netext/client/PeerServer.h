7fed22a59bc28cd174138c5cbd9ec250588eeac1
Yahel Hirom <yahel.h0505@gmail.com>
2022-12-10 12:10:40 +0200

    Added Basic Headers(NOT DONE) + gitignore issue

---
+++ netext/client/PeerServer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <iostream>
+#include <string>
+#include <WinSock2.h>
+
+using std::string;
+
+
+class PeerServer 
+{
+	public:
+		PeerServer();
+		~PeerServer();
+
+		void bindAndListen();
+
+
+	private:
+
+	
+};
