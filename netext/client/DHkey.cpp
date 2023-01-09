4a6aabd7346e551db7c6a3ff52b71d290487b074
avnogy <avner.mindelis@gmail.com>
2023-01-09 20:36:29 +0200

    implemented generate prime

--- netext/client/DHkey.cpp
+++ netext/client/DHkey.cpp
@@ -1,5 +1,24 @@
 #include "DHkey.h"
 
+
 DHkey::DHkey()
 {
+	srand(time(nullptr));
+	_kPrivate = rand() % (_p - MINIUM_VALUE) + MINIUM_VALUE;
+
+	_p = generatePrime();
+	_g = generatePrimitiveRoot();
+}
+
+int DHkey::generatePrime() const
+{
+	srand(time(nullptr));
+	int candidate = rand();
+
+	while (!isPrime(candidate))
+	{
+		candidate = rand();
+	}
+
+	return candidate;
 }
--- netext/client/DHkey.h
+++ netext/client/DHkey.h
@@ -1,22 +1,22 @@
 #pragma once
+#include <iostream>
 
-
-
+#define MINIUM_VALUE 2
 class DHkey
 {
 	public:
 
 		DHkey();
 
-		int calculateSharedKey();
+		int calculateSharedKey(const int otherKey) const;
 
-		void generatePrimeAndPrimitive();
-
-		int modular_pow(int base, int exponent, int modulus);
-		bool isPrime(int num);
+		int modular_pow(const int base, const int exponent, const int modulus) const ;
+		int generatePrime() const;
+		int generatePrimitiveRoot() const;
+		bool isPrime(int num) const;
 		bool isPrimitiveRoot(int num, int modulo);
 
-
+		
 
 	private:
 		int _p; // modulus (public)
