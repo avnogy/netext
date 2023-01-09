43cda6b8e048b1576b4a1858f0582424df52fa47
avnogy <avner.mindelis@gmail.com>
2023-01-09 20:49:27 +0200

    implemented calculate shared key

--- netext/client/DHkey.h
+++ netext/client/DHkey.h
@@ -4,24 +4,24 @@
 #define MINIUM_VALUE 2
 class DHkey
 {
-	public:
+public:
 
-		DHkey();
+	DHkey();
 
-		int calculateSharedKey(const int otherKey) const;
+	int calculateSharedKey(const int otherKey) const;
 
-		int modular_pow(const int base, const int exponent, const int modulus) const ;
-		int generatePrime() const;
-		int generatePrimitiveRoot() const;
-		bool isPrime(int num) const;
-		bool isPrimitiveRoot(int num, int modulo);
+	int modular_pow(const int base, const int exponent, const int modulus) const;
+	int generatePrime() const;
+	int generatePrimitiveRoot() const;
+	bool isPrime(int num) const;
+	bool isPrimitiveRoot(int num, const int modulo) const;
 
-		
 
-	private:
-		int _p; // modulus (public)
-		int _g; // primitive root (public)
 
-		int _kPrivate; // private key (for the specific client)
+private:
+	int _p; // modulus (public)
+	int _g; // primitive root (public)
+
+	int _kPrivate; // private key (for the specific client)
 
 };
