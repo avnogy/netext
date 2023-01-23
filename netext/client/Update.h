d0c2cda9c5dc6203214ba529c2ecf66ebd84d3a1
avnogy <avner.mindelis@gmail.com>
2023-01-23 20:05:24 +0200

    created update class

---
+++ netext/client/Update.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Helper.h"
+
+class Update
+{
+private:
+	string sender_id;
+	string updateCode;
+	int editLocation;
+	string updateData;
+	string timestamp;
+public:
+	Update(const string sender_id,
+		const string updateCode,
+		const int editLocation,
+		const string updateData,
+		const string timestamp);
+	~Update();
+};
