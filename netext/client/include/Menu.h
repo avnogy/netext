bb92ec55258012c0040c6dc3fb163124f87d56de
avnogy <avner.mindelis@gmail.com>
2023-02-18 23:08:57 +0200

    implemented menu class

--- netext/client/include/Menu.h
+++ netext/client/include/Menu.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "Utilities.h"
 
+#define WELCOME_SCREEN "Menu:\n1. Create Session\n2. Join Session\n3. Delete Session(IF ALREADY EXISTED)"
+#define INVALID_CHOICE "Invalid Choice, Try again: "
 class Menu
 {
+public:
+	static void mainMenu();
+	static bool HandleChoice(int choice);
+	static enum Choices { CREATE_SESSION = 1, JOIN_SESSION, DELETE_SESSION };
 };
 
--- netext/client/src/Menu.cpp
+++ netext/client/src/Menu.cpp
@@ -1,1 +1,30 @@
 #include "include/Menu.h"
+
+void Menu::mainMenu()
+{
+    cout << WELCOME_SCREEN << endl << "Option: ";
+	while (!HandleChoice(Helper::getInt()))
+	{
+		cout << INVALID_CHOICE;
+	}
+}
+
+bool Menu::HandleChoice(int choice)
+{
+	switch (choice)
+	{
+	case Menu::Choices::CREATE_SESSION:
+		//peerserver create
+		break;
+	case Menu::Choices::JOIN_SESSION:
+		//peerclient join
+		break;
+	case Menu::Choices::DELETE_SESSION:
+		//peerserver delete
+		break;
+	default:
+		return FAILURE;
+		break;
+	}
+	return SUCCESS;
+}
